0c97401839bb2b5699be1b90c368664d65c3db8c
Oluwasegun Ige <mailshegze@gmail.com>
2025-07-26 16:42:22 +0100

    feat: implement route protection

--- app/routes/tasks.py
+++ app/routes/tasks.py
@@ -1,16 +1,22 @@
+from typing import Annotated
+
 from fastapi import APIRouter, Response, Depends, status, HTTPException
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from sqlmodel import Session
 
 from app.database import get_session
 from app.models.task import Task
+from app.models.user import User
 from app.utils.database_utils import save_to_database
+from app.utils.user_utils import get_current_user
 
 router = APIRouter()
 
 @router.post("/api/tasks", tags=["tasks"])
-def create_task(
+async def create_task(
     task: Task, 
-    response: Response, 
+    response: Response,
+    current_user: User = Depends(get_current_user),
     session: Session = Depends(get_session)
 ):
     try:
--- app/utils/user_utils.py
+++ app/utils/user_utils.py
@@ -1,7 +1,43 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+import jwt
+from jwt.exceptions import InvalidTokenError
+from sqlmodel import Session
+
+from app.config import settings
+from app.database import get_session
 from app.models.user import User, UserLogin
 from app.utils.auth_utils import generate_password_hash
+
+ALGORITHM = settings.algorithm
+SECRET_KEY = settings.secret_key
+
+security = HTTPBearer()
 
 def build_user_model(user: UserLogin) -> User:
     user_dict = user.model_dump()
     user_dict["hashed_password"] = generate_password_hash(user.password)
     return User(**user_dict)
+
+async def get_current_user(
+        credentials: HTTPAuthorizationCredentials = Depends(security),
+        session: Session = Depends(get_session)
+):
+    token = credentials.credentials
+
+    credentials_exception = HTTPException(
+        status.HTTP_401_UNAUTHORIZED,
+        "Could not validate credentials",
+        {"WWW-Authenticate": "Bearer"}
+    )
+
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        user = session.get(User, username)
+        return user
+    except InvalidTokenError:
+        raise credentials_exception
+    
