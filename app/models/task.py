e296f017ad1f12d50e731781b75535c84ec25f2c
Oluwasegun Ige <mailshegze@gmail.com>
2025-08-16 23:40:34 +0100

    set dueDate alias in task model

--- app/models/task.py
+++ app/models/task.py
@@ -7,7 +7,7 @@ class Task(SQLModel, table=True):
     title: str = Field()
     description: str | None = Field()
     is_completed: bool = Field(default=False)
-    due_date: datetime | None = Field()
+    due_date: datetime | None = Field(alias="dueDate")
     created_at: datetime = Field(default=datetime.now())
     updated_at: datetime = Field(default=datetime.now())
     owner: str = Field(foreign_key="user.username")
