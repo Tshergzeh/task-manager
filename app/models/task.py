f4f9634ed04c71d43bb812cbc26ff59a6668585d
Oluwasegun Ige <mailshegze@gmail.com>
2025-08-17 22:23:28 +0100

    fix: fix updated_at field on Task model

--- app/models/task.py
+++ app/models/task.py
@@ -1,6 +1,6 @@
-from datetime import datetime
+from datetime import datetime, timezone
 
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, SQLModel, Column, DateTime
 
 class Task(SQLModel, table=True):
     id: int = Field(primary_key=True, index=True)
@@ -8,11 +8,27 @@ class Task(SQLModel, table=True):
     description: str | None = Field()
     is_completed: bool = Field(default=False)
     due_date: datetime | None = Field(alias="dueDate")
-    created_at: datetime = Field(default=datetime.now())
-    updated_at: datetime = Field(default=datetime.now())
+
+    created_at: datetime = Field(
+        sa_column=Column(
+            DateTime(timezone=True), 
+            default=lambda: datetime.now(timezone.utc)
+        )
+    )
+
+    updated_at: datetime = Field(
+        sa_column=Column(
+            DateTime(timezone=True), 
+            default=lambda: datetime.now(timezone.utc), 
+            onupdate=lambda: datetime.now(timezone.utc)
+        )
+    )
+    
     owner: str = Field(foreign_key="user.username")
 
 class TaskComplete(SQLModel):
     id: int
     is_completed: bool = True
-    updated_at: datetime = datetime.now()
+    updated_at: datetime = Field(
+        default_factory=lambda: datetime.now(timezone.utc)
+    )
